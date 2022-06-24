9f4fbb393d52d6644e032d368f50f4cd0f545e70
AirinCompSci <airincompsci@gmail.com>
2022-06-24 15:48:00 -0400

    Webscraper only fully works every other run

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from time import sleep
 
 def column_extractor(list, keyword, soup):
   """Takes a soup, list, and keyword and returns a list of extracted data"""
@@ -18,10 +19,20 @@ def table_extractor(url, all_lists, keyword_list):
   df = df.T
   df.columns = keyword_list
   return df
-  
+ 
+keyword_list = ["wins", "draws", "losses", "goals_for", "goals_against", "points_avg"] 
 wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
 all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
-keyword_list = ["wins", "draws", "losses", "goals_for", "goals_against", "points_avg"]
-url = "https://fbref.com/en/comps/9/Premier-League-Stats"
-overall_20_21 = table_extractor(url, all_lists, keyword_list)
-print(overall_20_21)
+overall_21_22 = table_extractor("https://fbref.com/en/comps/9/Premier-League-Stats", all_lists, keyword_list)
+wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
+all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
+overall_20_21 = table_extractor("https://fbref.com/en/comps/9/10728/2020-2021-Premier-League-Stats", all_lists, keyword_list)
+wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
+all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
+overall_19_20 = table_extractor("https://fbref.com/en/comps/9/3232/2019-2020-Premier-League-Stats", all_lists, keyword_list)
+wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
+all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
+overall_18_19 = table_extractor("https://fbref.com/en/comps/9/1889/2018-2019-Premier-League-Stats", all_lists, keyword_list)
+sleep(1)
+overall_df = pd.concat([overall_21_22, overall_20_21, overall_19_20, overall_18_19])
+print(overall_df)
