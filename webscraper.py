bc1df1ed7fa200e933cd635775155bf9099e672d
AirinCompSci <airincompsci@gmail.com>
2022-06-24 14:09:31 -0400

    Webscraper column_extractor function added

--- webscraper.py
+++ webscraper.py
@@ -1,10 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
+def column_extractor(list, keyword):
+  """Takes a list and fills it with the string keyword found on fbref.com"""
+  for element in soup.find_all('td', {"data-stat": keyword}):
+    list.append(element.text)
+  return list
 
 URL = "https://fbref.com/en/comps/9/Premier-League-Stats"
 r = requests.get(URL)
 
 soup = BeautifulSoup(r.content, 'html5lib')
 
-for element in soup.find_all('td', {"data-stat": "wins"}):
-  print(element.text)
+wins = []
+#for element in soup.find_all('td', {"data-stat": "wins"}):
+#  wins.append(element.text)
+  
+win_column = column_extractor(wins, "wins")
+print(win_column)
