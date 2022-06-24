2780b4ae9690f3a57e23319c40fd614585ab31b9
AirinCompSci <airincompsci@gmail.com>
2022-06-24 14:36:47 -0400

    Created table_extractor function

--- webscraper.py
+++ webscraper.py
@@ -2,20 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def column_extractor(list, keyword):
-  """Takes a list and fills it with the string keyword found on fbref.com"""
+def column_extractor(list, keyword, soup):
+  """Takes a soup, list, and keyword and returns a list of extracted data"""
   for element in soup.find_all('td', {"data-stat": keyword}):
     list.append(element.text)
   return list
 
-URL = "https://fbref.com/en/comps/9/Premier-League-Stats"
-r = requests.get(URL)
-
-soup = BeautifulSoup(r.content, 'html5lib')
-
-wins = []
-#for element in soup.find_all('td', {"data-stat": "wins"}):
-#  wins.append(element.text)
+def table_extractor(url, all_lists, keyword_list):
+  """Takes a fbref.com URL and creates a DF from the given year/statistics"""
+  r = requests.get(url)
+  soup = BeautifulSoup(r.content, 'html5lib')
+  for list, keyword in zip(all_lists, keyword_list):
+    column_extractor(list, keyword, soup)
+  df = pd.DataFrame(all_lists)
+  df = df.T
+  df.columns = keyword_list
+  return df
   
-win_column = column_extractor(wins, "wins")
-print(win_column)
+wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
+all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
+keyword_list = ["wins", "draws", "losses", "goals_for", "goals_against", "points_avg"]
+url = "https://fbref.com/en/comps/9/Premier-League-Stats"
+overall_20_21 = table_extractor(url, all_lists, keyword_list)
+print(overall_20_21)
