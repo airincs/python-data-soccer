1ddd014fc17b2e61348feb0db41517cffdf9e927
AirinCompSci <airincompsci@gmail.com>
2022-06-25 15:11:30 -0400

    Webscraper properly scrapes and creates CSV files

--- webscraper.py
+++ webscraper.py
@@ -21,37 +21,40 @@ def table_extractor(url, all_lists, keyword_list):
   return df
 
 ## Gathering 'summary' statistics
-#keyword_list = ["wins", "draws", "losses", "goals_for", "goals_against", "points_avg"] 
-#wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
-#all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
-#overall_21_22 = table_extractor("https://fbref.com/en/comps/9/Premier-League-Stats", all_lists, keyword_list)
-#wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
-#all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
-#overall_20_21 = table_extractor("https://fbref.com/en/comps/9/10728/2020-2021-Premier-League-Stats", all_lists, keyword_list)
-#wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
-#all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
-#overall_19_20 = table_extractor("https://fbref.com/en/comps/9/3232/2019-2020-Premier-League-Stats", all_lists, keyword_list)
-#wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
-#all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
+keyword_list = ["wins", "draws", "losses", "goals_for", "goals_against", "points_avg"] 
+wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
+all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
+overall_21_22 = table_extractor("https://fbref.com/en/comps/9/Premier-League-Stats", all_lists, keyword_list)
+wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
+all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
+overall_20_21 = table_extractor("https://fbref.com/en/comps/9/10728/2020-2021-Premier-League-Stats", all_lists, keyword_list)
+wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
+all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
+overall_19_20 = table_extractor("https://fbref.com/en/comps/9/3232/2019-2020-Premier-League-Stats", all_lists, keyword_list)
+wins, draws, losses, goals_for, goals_against, points_avg = ([] for i in range(6))
+all_lists = (wins, draws, losses, goals_for, goals_against, points_avg)
 
-#overall_df = pd.concat([overall_21_22, overall_20_21, overall_19_20])
+overall_df = pd.concat([overall_21_22, overall_20_21, overall_19_20])
+overall_df.to_csv("overallDF.csv", encoding='utf-8', index=False)
+
 
 ## Gathering 'shooting' statistics
-#keyword_list = ["goals", "shots_total_per90", "goals_per_shot", "average_shot_distance"]
-#goals, shots_total_per90, goals_per_shot, average_shot_distance = ([] for i in range(4))
-#all_lists = (goals, shots_total_per90, goals_per_shot, average_shot_distance)
-#shooting_21_22 = table_extractor("https://fbref.com/en/comps/9/shooting/Premier-League-Stats", all_lists, keyword_list)
-#shooting_21_22 = shooting_21_22.iloc[:20]
-#goals, shots_total_per90, goals_per_shot, average_shot_distance = ([] for i in range(4))
-#all_lists = (goals, shots_total_per90, goals_per_shot, average_shot_distance)
-#shooting_20_21 = table_extractor("https://fbref.com/en/comps/9/10728/shooting/2020-2021-Premier-League-Stats", all_lists, keyword_list)
-#shooting_20_21 = shooting_20_21.iloc[:20]
-#goals, shots_total_per90, goals_per_shot, average_shot_distance = ([] for i in range(4))
-#all_lists = (goals, shots_total_per90, goals_per_shot, average_shot_distance)
-#shooting_19_20 = table_extractor("https://fbref.com/en/comps/9/3232/shooting/2019-2020-Premier-League-Stats", all_lists, keyword_list)
-#shooting_19_20 = shooting_19_20.iloc[:20]
+keyword_list = ["goals", "shots_total_per90", "goals_per_shot", "average_shot_distance"]
+goals, shots_total_per90, goals_per_shot, average_shot_distance = ([] for i in range(4))
+all_lists = (goals, shots_total_per90, goals_per_shot, average_shot_distance)
+shooting_21_22 = table_extractor("https://fbref.com/en/comps/9/shooting/Premier-League-Stats", all_lists, keyword_list)
+shooting_21_22 = shooting_21_22.iloc[:20]
+goals, shots_total_per90, goals_per_shot, average_shot_distance = ([] for i in range(4))
+all_lists = (goals, shots_total_per90, goals_per_shot, average_shot_distance)
+shooting_20_21 = table_extractor("https://fbref.com/en/comps/9/10728/shooting/2020-2021-Premier-League-Stats", all_lists, keyword_list)
+shooting_20_21 = shooting_20_21.iloc[:20]
+goals, shots_total_per90, goals_per_shot, average_shot_distance = ([] for i in range(4))
+all_lists = (goals, shots_total_per90, goals_per_shot, average_shot_distance)
+shooting_19_20 = table_extractor("https://fbref.com/en/comps/9/3232/shooting/2019-2020-Premier-League-Stats", all_lists, keyword_list)
+shooting_19_20 = shooting_19_20.iloc[:20]
 
-#shooting_df = pd.concat([shooting_21_22, shooting_20_21, shooting_19_20])
+shooting_df = pd.concat([shooting_21_22, shooting_20_21, shooting_19_20])
+shooting_df.to_csv("shootingDF.csv", encoding='utf-8', index=False)
 
 ## Gathering 'passing' statistics
 keyword_list = ["passes_completed", "passes_pct", "assisted_shots", "passes_completed_short"]
@@ -69,3 +72,4 @@ passing_19_20 = table_extractor("https://fbref.com/en/comps/9/3232/passing/2019-
 passing_19_20 = passing_21_22.iloc[:20]
 
 passing_df = pd.concat([passing_21_22, passing_20_21, passing_19_20])
+passing_df.to_csv("passingDF.csv", encoding='utf-8', index=False)
